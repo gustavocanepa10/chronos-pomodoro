f56d1eb03219854df7976cdfe6afeef1c495cb96
Gustavo <gustavociscotto@gmail.com>
2025-06-18 22:49:38 -0300

    att notes

--- src/Contexts/TaskContext/index.tsx
+++ src/Contexts/TaskContext/index.tsx
@@ -1,19 +1,40 @@
-import { createContext, useState, type SetStateAction } from "react";
+import {createContext,useContext,useState,type SetStateAction} from "react";
 import type { TaskStateModel } from "../../models/TaskStateModel";
-import { initialTaskState } from "./InitialTaskState";
 
+
+
+// Tipo de dentro do Contexto
 type TaskContextProps = {
   state: TaskStateModel;
   setState: React.Dispatch<SetStateAction<TaskStateModel>>;
 };
 
 
+// Tipagem e definição do estado inicial
+const initialTaskState: TaskStateModel = {
+  tasks: [],
+  activeTask: null,
+  config: {
+    shortBreakTime: 5,
+    workTime: 25,
+    longBreakTime: 15,
+  },
+  currentCycle: 0,
+  formattedSecondsRemaining: "00:00",
+  secondsRemaining: 0,
+};
+
+
 
 const initialContextValue = {
-state: initialTaskState,
-  setState: () => {}
-}
+  state: initialTaskState,
+  setState: () => {},
+};
 
+
+
+// TaskContextProps que é o tipo que o contexto vai receber como valor
+// initialContextValue é o valor padrão se o mesmo não tiver nenhum valor
 export const TaskContext = createContext<TaskContextProps>(initialContextValue);
 
 
@@ -22,11 +43,10 @@ type TaskContextProviderProps = {
   children: React.ReactNode;
 };
 
-
-
-
-
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
+
+
+    // valor inicial do state 
   const [state, setState] = useState(initialTaskState);
 
   return (
@@ -34,4 +54,8 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
       {children}
     </TaskContext.Provider>
   );
+}
+
+export function UseTaskContext() {
+  return useContext(TaskContext);
 }
